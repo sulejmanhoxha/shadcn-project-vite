2a82baba30c591003706af311130a8fb25dd5812
Sulejman Hoxha <sulejman.hoxha2001@hotmail.com>
2023-07-16 02:39:38 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import { Button } from "@/components/ui"
-
+import { Button } from "@/components/ui/button";
 
 function App() {
-
-
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-          <Button>Click me</Button>
-
-    </>
-  )
+	return (
+		<>
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+			<Button>Click me</Button>
+		</>
+	);
 }
 
-export default App
+export default App;
