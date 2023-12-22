ee00f466d1b5a5d402cf9d5e086c4c5e2441f563
sulejmanhoxha <sulejman.hoxha2001@hotmail.com>
2023-12-22 23:30:28 +0100

    update to latest versions

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,45 @@
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 function App() {
-	return (
-		<>
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
-			<Button>Click me</Button>
-		</>
-	);
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-red-700 text-2xl">Hello World</h1>
+      <Button>Click me</Button>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
 
 export default App;
